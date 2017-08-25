077348cc2839251a9ee80c41b27efa9ce43d7c19
Max Scher <maxobaxo@gmail.com>
2017-08-25 10:04:36 -0700

    list all players in list.component using player.service methods

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player.model';
+import { Router } from '@angular/router';
+import { PlayerService } from '../player.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [PlayerService]
 })
+
 export class ListComponent implements OnInit {
   players: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.players = this.playerService.getPlayers();
   }
 
 }
