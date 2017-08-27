8c4514fb2afe0fca53cf5601e8e5718630ad8554
Max Scher <maxobaxo@gmail.com>
2017-08-26 21:03:46 -0700

    fix issue with retrieving timestamp and display it properly

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -16,6 +16,7 @@ export class ForumComponent implements OnInit {
   messages: FirebaseListObservable<any[]>;
   currentRoute = this.router.url;
   addingNewMessage = false;
+  timestamp = new Date();
 
   constructor(private router: Router, private messageService: MessageService) { }
 
@@ -27,13 +28,20 @@ export class ForumComponent implements OnInit {
     this.addingNewMessage = true;
   }
 
-  createTimestamp() {
-    var now = new Date();
-    var timestamp = now.getDate().toString() + "." + now.getMonth().toString() + "." + now.getFullYear().toString();
-    return timestamp
-  }
-
   submitForm(author: string, content: string, date: string) {
+    var day = this.timestamp.getDate().toString();
+    var month = this.timestamp.getMonth().toString();
+    var year = this.timestamp.getFullYear().toString();
+
+    var hours = this.timestamp.getHours();
+    var minutes = this.timestamp.getMinutes();
+    var ampm = hours>= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? 0+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
+    date = month + "/" + day + "/" + year + ', ' + strTime;
     var newMessage = new Message(author, content, date);
     this.messageService.addNewMessage(newMessage);
     this.addingNewMessage = false;
